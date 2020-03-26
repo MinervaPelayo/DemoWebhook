7d5b509d95c2a85ce90249d36d4bb690f7cf397d
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2020-03-26 14:23:09 +0200

    Edit package.json and added Procfile

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
