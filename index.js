13e79a3e91cdce4dde0bd5cd9a0b6b6df1159df6
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2020-03-27 14:23:49 +0200

    Edit configuration

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json()); // creates express http server
+require('dotenv').config()
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
--- src/constants.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').load()
-
-const verify_token = process.env.VERIFY_TOKEN;
-
-const page_access_token = process.env.PAGE_ACCESS_TOKEN;
-
-export { verify_token, page_access_token };
