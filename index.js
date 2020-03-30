c3b9288c0c4bc4f9f7092047cf1862e887890948
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2020-03-30 17:55:51 +0300

    Webhook response

--- index.js
+++ index.js
@@ -61,10 +61,9 @@ app.post("/webhook", (req, res) => {
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       }
-
-      // Returns a '200 OK' response to all requests
-      res.status(200).send("EVENT_RECEIVED");
     });
+    // Returns a '200 OK' response to all requests
+    res.sendStatus(200);
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
