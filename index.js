547993628c21b8ff06dd5b6894ee9e4a67a9c2c8
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2020-03-31 08:43:07 +0300

    Webhook response

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post("/webhook", (req, res) => {
     body.entry.forEach(function(entry) {
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
+    /*  let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
       // Get the sender PSID
@@ -62,7 +62,28 @@ app.post("/webhook", (req, res) => {
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
-      }
+      }               */
+
+
+
+ // Iterate over each messaging event
+ entry.messaging.forEach((messagingEvent) => {
+  console.log({messagingEvent});
+
+  let sender_psid = messagingEvent.sender.id;
+  console.log("Sender PSID: " + sender_psid);
+  if (messagingEvent.message) {
+    handleMessage(messagingEvent, sender_psid);
+  } else {
+    console.error(
+      'Webhook received unknown messagingEvent: ',
+      messagingEvent
+    );
+  }
+});
+
+
+
     });
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
